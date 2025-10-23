9748c01b0fc42140a5038d431b4aaf967ef9029c
John Nettles <chiefsquanto22@ksu.edu>
2025-10-23 12:40:42 -0500

    barely added anything lol

--- EmeraldEweRPi.py
+++ EmeraldEweRPi.py
@@ -1,1 +1,18 @@
 print("This is the start")
+
+from square import Square
+import gpiozero
+# total number of balls of yarn sold
+total = 0
+
+# receive order event from Square
+
+# check for any balls of yarn in orders
+newSold = 0
+
+total+=newSold
+print("New order!")
+print(f"Total: {total}")
+
+#update ticker counter
+
