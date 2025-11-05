e3f8fd3d14736b6fcd701004b8b75f444f399c1d
John Nettles <chiefsquanto22@ksu.edu>
2025-11-04 22:00:54 -0600

    Added .env, set webhook get to square SDK sandbox

--- EmeraldEweRPi.py
+++ EmeraldEweRPi.py
@@ -1,18 +1,59 @@
 print("This is the start")
-
 from square import Square
-import gpiozero
+from square.environment import SquareEnvironment
+from square.core.api_error import ApiError
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = Square(
+    environment=SquareEnvironment.SANDBOX,
+    token = os.environ['SQUARE_ACCESS_TOKEN'])
+    
+try:
+    response = client.locations.list()
+    for location in response.locations:
+        print(f"{location.id}: ", end ="")
+        print(f"{location.name}: ", end ="")
+        print(f"{location.address}: ", end ="")
+except ApiError as e:
+    for error in e.errors:
+        print(error.category)
+        print(error.code)
+        print(error.detail)
+        
+
 # total number of balls of yarn sold
-total = 0
+
 
 # receive order event from Square
 
 # check for any balls of yarn in orders
-newSold = 0
 
-total+=newSold
-print("New order!")
-print(f"Total: {total}")
 
 #update ticker counter
 
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST']) #'/webhook' needs to be a URL that will then route here
+
+
+
+
+def handle_webhook():
+    if request.method == 'POST':
+        try:
+            payload = request.json
+            print(f"Received this payload: {payload}")
+            return jsonify({"message": "Webhook received successfully"}), 205
+        except Exception as e:
+            print(f"Error: {e}")
+            return jsonify ({"error":"Failed to process webhook"}), 401
+    else:
+        return jsonify({"message": "method now allowed"}), 406
+        
+if __name__ == '__main__':
+    app.run(debug=True)
